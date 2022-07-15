a55ea8c7a645bfbdae721b35da51dd4f0efcc231
Oleg Tsvetkov <oleg@iprally.com>
2022-07-15 08:34:11 +0300

    Add idea configs

---
+++ test/pdf_gen_performance/core_test.clj
@@ -0,0 +1,7 @@
+(ns pdf-gen-performance.core-test
+  (:require [clojure.test :refer :all]
+            [pdf-gen-performance.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
